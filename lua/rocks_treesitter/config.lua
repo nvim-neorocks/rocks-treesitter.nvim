9bd981bc761b7031d95935ea2e7f6e0285b46950
Marc Jakobi <marc.jakobi@tiko.energy>
2024-08-28 10:18:28 +0200

    feat: more fine-grained control for disabling highlighting

--- lua/rocks_treesitter/config.lua
+++ lua/rocks_treesitter/config.lua
@@ -7,6 +7,8 @@ local config = {
     auto_install = "prompt",
     ---@type "all" | table<string, boolean>
     auto_highlight = {},
+    ---@type table<string, boolean> | fun(lang: string, bufnr: integer):boolean
+    disable = {},
     ---@type table<string, string>
     parser_map = require("rocks_treesitter.ft_parser_map"),
 }
@@ -14,9 +16,22 @@ local config = {
 local api = require("rocks.api")
 
 ---@class RocksTreesitterOpts
----@field auto_highlight? string[] | "all" Parsers to automatically enable syntax highlighting for. Note that these are parser languages, not file types. Defaults to an empty list.
----@field auto_install? boolean | "prompt" Auto-install parsers in `auto_highlight` as needed? Default: 'prompt'
----@field parser_map? table<string, string> Map from filetypes to parser languages
+---
+--- Parsers to automatically enable syntax highlighting for.
+--- Note that these are parser languages, not file types.
+--- Defaults to an empty list.
+---@field auto_highlight? string[] | "all"
+---
+--- Auto-install parsers in `auto_highlight` as needed?
+--- Default: 'prompt'
+---@field auto_install? boolean | "prompt"
+---
+--- Map from filetypes to parser languages
+---@field parser_map? table<string, string>
+---
+--- File types to disable highlighting for (lua + toml config)
+--- or a function to conditionally disable highlighting (lua only)
+---@field disable? string[] | fun(lang: string, bufnr: integer):boolean
 
 local toml_config = api.get_rocks_toml()["treesitter"]
 ---@type RocksTreesitterOpts
@@ -32,13 +47,32 @@ if type(lua_config) == "table" then
     opts = vim.tbl_deep_extend("force", opts, lua_config)
 end
 
+---@generic T
+---@param lst? T[]
+---@return table<T, boolean>
+local function list_to_bool_map(lst)
+    return vim
+        .iter(lst or {})
+        ---@generic T
+        ---@param acc table<T, boolean>
+        ---@param val T
+        :fold(
+            {},
+            function(acc, val)
+                acc[val] = true
+                return acc
+            end
+        )
+end
+
 --- Map opts to configs, preserving defaults if not set
 config.auto_highlight = opts.auto_highlight == "all" and "all"
-    or vim.iter(opts.auto_highlight or {}):fold({}, function(acc, lang)
-        ---@cast lang string
-        acc[lang] = true
-        return acc
-    end)
+    ---@diagnostic disable-next-line: param-type-mismatch
+    or list_to_bool_map(opts.auto_highlight)
+config.disable = type(opts.disable) == "function" and opts.disable
+    ---@diagnostic disable-next-line: param-type-mismatch
+    or list_to_bool_map(opts.disable)
+---@diagnostic disable-next-line: assign-type-mismatch
 config.auto_install = vim.F.if_nil(opts.auto_install, config.auto_install)
 config.parser_map = vim.tbl_extend("force", config.parser_map, opts.parser_map or {})
 
